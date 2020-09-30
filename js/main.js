4525ea1854960fd652d9f1be575591db5c881421
aderose <sandro.derose28@yahoo.com>
2020-09-30 18:40:21 +0100

    Isolate Tile factory, pull tileValue into Tile object,  update click listener to single click

--- js/main.js
+++ js/main.js
@@ -1,45 +1,76 @@
 const gameBoard = (function () {
   const boardContainer = document.querySelector(".gameboard");
-  const tileValues = ["x", "x", "o", "o", "x", "o", "o", "x", "o"];
-  const tiles = [];
+  const tileObjects = [];
+  const BOARD_SIZE = 9;
+
+  // initialise the game board with empty tiles
+  function initialise() {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      tileObjects[i] = createTile();
+    }
+    render();
+    return tileObjects;
+  }
 
   // render the gameBoard
   function render() {
-    tileValues.forEach((tileValue, index) => {
-      addToBoard(tileValue, index);
+    tileObjects.forEach((tileObject) => {
+      tileObject.update();
+      tileObject.render(boardContainer);
     });
   }
 
-  // add an individual tile to the board
-  function addToBoard(tileValue, tileIndex) {
-    tiles[tileIndex] = createTile(tileValue);
-    boardContainer.appendChild(tiles[tileIndex]);
-  }
-
-  // create a tile object for the given tilevalue
-  function createTile(tileValue) {
-    const tile = document.createElement("div");
-    tile.classList.add("tile", "round-border");
-    const icon = document.createElement("i");
-    if (tileValue === "x") icon.classList.add("fas", "fa-times");
-    if (tileValue === "o") icon.classList.add("far", "fa-circle");
-    tile.appendChild(icon);
-    return tile;
-  }
-
-  return { tileValues, tiles, render };
+  return { initialise, render };
 })();
 
 const gameController = (function () {
-  gameBoard.render();
+  const tileObjects = gameBoard.initialise();
 
-  const tiles = gameBoard.tiles;
-  const tileValues = gameBoard.tileValues;
+  // get input from player on what game type to play
+  // singleplayer/multiplayer
+  // make players
 
-  // listen for click on tile
-  tiles.forEach((tile, index) => {
-    tile.addEventListener("click", () => {
-      console.log("Clicked: " + tileValues[index]);
-    });
-  });
+  // game running
+  // listen for a single click on tile
+  tileObjects.forEach((tileObject) =>
+    tileObject.tile.addEventListener("click", makeTurn.bind(tileObject), {
+      once: true,
+    })
+  );
+
+  function makeTurn() {
+    // work out which player's turn it is
+    // set that tile to the players icon
+    this.setIcon("x");
+    // check if game is over
+  }
 })();
+
+// create a tile object for the given tilevalue
+function createTile() {
+  let value = "";
+  const tile = document.createElement("div");
+  tile.classList.add("tile", "round-border", "clickable");
+  const icon = document.createElement("i");
+
+  // set icon and prevent the tile from being clicked & update icon
+  const setIcon = (tileValue) => {
+    value = tileValue;
+    tile.classList.remove("clickable");
+    update();
+  };
+
+  const getIcon = () => value;
+
+  const update = () => {
+    if (value === "x") icon.classList.add("fas", "fa-times");
+    if (value === "o") icon.classList.add("far", "fa-circle");
+  };
+
+  const render = (board) => {
+    board.appendChild(tile);
+  };
+
+  tile.appendChild(icon);
+  return { tile, setIcon, getIcon, update, render };
+}
